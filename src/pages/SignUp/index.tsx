d76d8d5233005d36970c8437cde264c52a841774
david <davidwdiniz@gmail.com>
2020-09-05 12:05:41 -0300

    Criação da página de cadastro / Utilização do unform

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {FiMail, FiLock, FiUser, FiArrowLeft} from "react-icons/fi";
+import {Form} from "@unform/web";
+
+import logoImg from "../../assets/logo.svg";
+import {Container, Content, Background} from "./styles";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+const SignUp: React.FC = () => {
+    function handleSubmit(data: object) {
+        console.log(data);
+    }
+
+    return (
+        <Container>
+            <Background />
+            <Content>
+                <img src={logoImg}  alt="GoBarber"/>
+                <Form onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
+                    <Input name="nome" icon={FiUser} placeholder="Nome" />
+                    <Input name="email" icon={FiMail} placeholder="Email"/>
+                    <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+                <a href="back">
+                    <FiArrowLeft />
+                    Voltar para logon
+                </a>
+            </Content>
+        </Container>
+    );
+};
+
+export default SignUp;
