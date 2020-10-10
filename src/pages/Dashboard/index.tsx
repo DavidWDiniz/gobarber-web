c3ffe52aee431065cd670b70f7398e2958d330de
david <davidwdiniz@gmail.com>
2020-10-10 15:23:32 -0300

    Próximo agendamento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 import logoImg from "../../assets/logo.svg";
-import {Container, Header, HeaderContent, Profile} from "./styles";
-import {FiPower} from "react-icons/all";
+import {Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Calendar} from "./styles";
+import {FiClock, FiPower} from "react-icons/all";
 import {useAuth} from "../../hooks/auth";
 
 const Dashboard: React.FC = () => {
@@ -24,6 +24,30 @@ const Dashboard: React.FC = () => {
                     </button>
                 </HeaderContent>
             </Header>
+            <Content>
+                <Schedule>
+                    <h1>Horários agendados</h1>
+                    <p>
+                        <span>Hoje</span>
+                        <span>Dia 06</span>
+                        <span>Segunda-feira</span>
+                    </p>
+                    <NextAppointment>
+                        <strong>Atendimento a seguir</strong>
+                        <div>
+                            <img src="https://e7.pngegg.com/pngimages/790/843/png-clipart-avatar-aang-aang-zuko-azula-katara-korra-aang-human-cartoons.png" alt="avatar" />
+                            <strong>Avatar</strong>
+                            <span>
+                                <FiClock />
+                                8:00
+                            </span>
+                        </div>
+                    </NextAppointment>
+                </Schedule>
+                <Calendar>
+
+                </Calendar>
+            </Content>
         </Container>
     );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,15 +1,15 @@
- import styled from "styled-components";
+import styled from "styled-components";
 
 export const Container = styled.div`
 
 `;
 
- export const Header = styled.header`
+export const Header = styled.header`
   padding: 32px 0;
   background: #28262E;
 `;
 
- export const HeaderContent = styled.div`
+export const HeaderContent = styled.div`
   max-width: 1120px;
   margin: 0 auto;
   display: flex;
@@ -32,7 +32,7 @@ export const Container = styled.div`
   }
 `;
 
- export const Profile = styled.div`
+export const Profile = styled.div`
   display: flex;
   align-items: center;
   margin-left: 80px;
@@ -58,4 +58,99 @@ export const Container = styled.div`
     }
   }
   
+`;
+
+export const Content = styled.main`
+  max-width: 1120px;
+  margin: 64px auto;
+  display: flex;
+`;
+
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+  
+  h1 {
+  font-size: 36px;
+  }
+  
+  p {
+    margin-top: 8px;
+    color: #FF9000;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+    
+    span {
+      display: flex;
+      align-items: center;
+    }
+    
+    span + span::before {
+      content: "";
+      width: 1px;
+      height: 12px;
+      background: #FF9000;
+      margin: 0 8px;
+    }
+  }
+  
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+  
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    font-weight: 400;
+  }
+  
+  div {
+    background: #3E3B47;
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-top: 24px;
+    position: relative;
+    
+    &::before {
+      position: absolute;
+      height: 80%;
+      width: 1px;
+      left: 0;
+      top: 10%;
+      content: "";
+      background: #FF9000;
+    }
+    
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+    
+    strong {
+      margin-left: 24px;
+      color: #FFF;
+    }
+    
+    span {
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+      color: #999591;
+    }
+    
+    svg {
+      color: #FF9000;
+      margin-right: 8px;
+    }
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 380px;
 `;
